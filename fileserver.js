067384666482bf234faea29f761c5f4d3e72855e
Cornelius Carstens <cornelius@awesome-boys.com>
2017-06-13 01:37:27 +0200

    basic changes on fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -1,8 +1,22 @@
 var express = require("express");
+var bodyparser = require("body-parser");
 
 var fileserver = express();
 
+fileserver.use(bodyparser.json());
+fileserver.use(bodyparser.urlencoded());
+
+
+fileserver.use(function (request, response, next) {
+    console.log(`${request.method} request for ${request.url}`);
+    next();
+});
+
 fileserver.use(express.static("./public_html"));
+
+fileserver.get("/get-phrases", function (request, response) {
+    response.json("hello");
+});
 
 fileserver.listen(3000);
 console.log("Express is running");
